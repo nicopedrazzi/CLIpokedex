f7591c50ab14ad498c3513c3f10c05f838646ce0
Nicolas Pedrazzi <nicolas.pedrazzi@hotmail.com>
2026-02-09 13:49:03 -0500

    added command line REPL

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { startREPL } from "./repl.js";
 
-const { createInterface } = require('node:readline');
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt:string,
-});
 
 function main() {
   startREPL();
--- src/repl.ts
+++ src/repl.ts
@@ -1,23 +1,31 @@
+import readline from "node:readline";
+
+
 export function cleanInput(input:string):string[]{
     return input .toLowerCase() .split(" ") .filter(Boolean);
 }
 
-export function startREPL(){
 
 
-const { createInterface } = require('node:readline');
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt:string,
-  
-  rl.on('line', (line) => {
-    if (!line){
-        rl.prompt;
-    };
-    cleanInput(line);
-  console.log(`Received: ${line}`);
-});
-});
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    const words = cleanInput(line);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
 }
 
